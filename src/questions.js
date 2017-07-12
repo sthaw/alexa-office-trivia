77274a09129384604232be1efd1c0fde19b06627
Su Thaw <thaws.su@gmail.com>
2017-07-11 23:56:24 -0700

    Add files via upload

---
+++ src/questions.js
@@ -0,0 +1,175 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_US" : [
+        {
+            "What is Meredith's last name?": [
+                "Palmer",
+                "Griffin",
+                "Garnet",
+                "Pepper",
+                "Paul",
+                "Garret"
+            ]
+        },
+        {
+            "Who's the only character that shares his name with the exact name of the actor?": [
+                "Creed",
+                "Meredith",
+                "Ryan",
+                "Pam",
+                "Kelly",
+                "Phyllis",
+                "Dwight"
+            ]
+        },
+        {
+            "Who does Michael impersonate that leads to the branch having Diversity Day?": [
+                "Chris Rock",
+                "Jackie Chan",
+                "Apu",
+                "OJ Simpson"
+            ]
+        },
+        {
+            "What was Kevin's nationality during Diversity Day?": [
+                "Italian",
+                "African American",
+                "Brazilian",
+                "Eskimo"
+            ]
+        },
+        {
+            "Where do Jim and Pam get married?": [
+                "Niagra Falls",
+                "Scranton",
+                "Chicago",
+                "Philadelphia"
+            ]
+        },
+        {
+            "What a cappella group did Andy sing in?": [
+                "Here Comes Treble",
+                "Pitches Be Crazy",
+                "Vocal Point",
+                "Aural Fixation"
+            ]
+        },
+        {
+            "How much money does Michael donate to Oscar's nephew's charity, not realizing it is a walk-a-thon and the amount is per mile?": [
+                "$25",
+                "$10",
+                "$40",
+                "$100"
+            ]
+        },
+        {
+            "In the season 2 Dundies, what Dundie award does Phyllis take home?": [
+                "The Bushiest Beaver",
+                "Best Mom",
+                "Creative Craftmaker",
+                "Whitest Sneakers"
+            ]
+        },
+        {
+            "What is on Todd Packer's vanity license plate?": [
+                "WLHUNG",
+                "LUVMKR",
+                "TODPKR",
+                "BGDADY"
+            ]
+        },
+        {
+            "What does Pam name 'box of paper snowshoe racing' during the Office Olympics?": [
+                "Flonkerton",
+                "Bixing",
+                "Pegerhosen",
+                "Snow show"
+            ]
+        },
+        {
+            "What are Meredith's five DVD choices for the game 'Desert Island'?": [
+                "Legends of the Fall, Legally Blonde, Bridges of Madison County, My Big Fat Greek Wedding, Ghost",
+                "The Shawshank Redemption, 40 Year Old Virgin, Gentlemen Prefer Blondes, Disney's Sleeping Beauty, Life of Pi",
+                "Gone With The Wind, The Burbs, Clerks II, Sense & Sensibility, Chronicles of Riddick",
+                "Fargo, Edward Scissor-hands, The Breakfast Club, Dazed and Confused, The Princess Bride"
+            ]
+        },
+        {
+            "In the season 2 Halloween episode, what was Jim's costume?": [
+                "Three Hole Punch Jim",
+                "Bookface",
+                "The rational consumer",
+                "Dave"
+            ]
+        },
+        {
+            "What is Dwight's Sensei's name in season 2?": [
+                "Ira",
+                "George",
+                "Stuart",
+                "Mr Miyagi"
+            ]
+        },
+        {
+            "Where do Jan and Michael take Christian, the Lackawanna County rep?": [
+                "Chili's",
+                "Applebee's",
+                "Olive Garden",
+                "Poor Richard's"
+            ]
+        },
+        {
+            "In season 2, who does Michael quickly dismiss saying 'You were totally satisfactory this year'?": [
+                "Angela",
+                "Oscar",
+                "Kevin",
+                "Dwight"
+            ]
+        },
+        {
+            "In season 2, who hosts a barbecue that Michael is not invited to?": [
+                "Jim",
+                "Pam",
+                "Phyllis",
+                "Oscar"
+            ]
+        },
+        {
+            "In the season 2 Christmas part, who ends up with the iPod at the end of the episode?": [
+                "Dwight",
+                "Pam",
+                "Ryan",
+                "Michael"
+            ]
+        },
+        {
+            "Who breaks up on the season 2 Booze Cruise?": [
+                "Jim and Katy",
+                "Pam and Roy",
+                "Kelly and Ryan",
+                "Michael and Jan"
+            ]
+        },
+        {
+            "In season 2, who is responsible for Michael's smelly carpet?": [
+                "Todd Packer",
+                "Dwight",
+                "Jim",
+                "Kevin"
+            ]
+        },
+        {
+            "What job does Jan encourage Pam to pursue in season 2?": [
+                "A design training program",
+                "A corporate receptionist job",
+                "A sales position",
+                "Michael's job"
+            ]
+        }
+    ]
+};
